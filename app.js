697ff28e833cecf4610e2450efc6470cb3ddd489
yudistiraardi <mail.ardix@gmail.com>
2023-09-11 09:23:49 +0700

    Update codes

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const btnPlus = document.querySelectorAll('.fa-plus-square');
 const btnMinus = document.querySelectorAll('.fa-minus-square');
 const question = document.querySelectorAll('.question');
 
-let cek;
-
     question.forEach(function(e, i) { 
-        cek = e.classList;
         btnPlus[i].addEventListener('click', function() {
             for (let x = 0; x < question.length; x++) {
                 question[x].classList.remove('show-text');
