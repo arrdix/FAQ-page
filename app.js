ba0397ba77ccf8506d0e2e9d60601a3aa16d282d
yudistiraardi <mail.ardix@gmail.com>
2023-09-11 10:01:15 +0700

    Update codes

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const btnPlus = document.querySelectorAll('.fa-plus-square');
 const btnMinus = document.querySelectorAll('.fa-minus-square');
 const question = document.querySelectorAll('.question');
 
-    question.forEach(function(e, i) { 
-        btnPlus[i].addEventListener('click', function() {
-            for (let x = 0; x < question.length; x++) {
-                question[x].classList.remove('show-text');
-            }
-            question[i].classList.add('show-text');
-        })
-
-        btnMinus[i].addEventListener('click', function() {
-            question[i].classList.remove('show-text');
-        })
-    });
+question.forEach(function(e, i) { 
+    btnPlus[i].addEventListener('click', function() {
+        for (let x = 0; x < question.length; x++) {
+            question[x].classList.remove('show-text');
+        }
+        question[i].classList.add('show-text');
+    })
+    btnMinus[i].addEventListener('click', function() {
+        question[i].classList.remove('show-text');
+    })
+});
